e8158049ffc0db5fb3a30412a0d91874f627bb3e
Julie Chu <julieychu@gmail.com>
2022-02-24 15:19:11 -0600

    add starting code, set up models

---
+++ models/Tag.js
@@ -0,0 +1,30 @@
+const { Model, DataTypes } = require('sequelize');
+
+const sequelize = require('../config/connection.js');
+
+// set up fields and rules for Tag model
+class Tag extends Model { }
+
+Tag.init(
+  {
+    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    tag_name: {
+      type: DataTypes.STRING,
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'tag',
+  },
+);
+
+module.exports = Tag;
