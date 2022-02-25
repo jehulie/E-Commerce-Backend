44d2cacc42a128d91afa117ba7d42a3a57464903
Julie Chu <julieychu@gmail.com>
2022-02-24 21:34:59 -0600

    update server and routes files

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,7 +3,7 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all categories
   // be sure to include its associated Products
   try {
@@ -21,7 +21,7 @@ router.get('/', (req, res) => {
   }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
     try {
@@ -44,7 +44,7 @@ router.get('/:id', (req, res) => {
     }
   });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
   try {
     const newCategory = await Category.create(req.body);
@@ -54,7 +54,7 @@ router.post('/', (req, res) => {
   }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
   try {
     const updateData = await Category.update(
@@ -70,13 +70,13 @@ router.put('/:id', (req, res) => {
       return;
     }
 
-    res.status(200).json(updateData);
+    res.status(200).json({message: 'The category was updated in the database.'});
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
   try {
     const deleteData = await Category.destroy({
